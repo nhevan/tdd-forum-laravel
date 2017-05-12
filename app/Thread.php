4dbc6cdbdee967f582ef7e6635f0ef73d56f959d
NH Evan <nhevan@gmail.com>
2017-05-12 21:08:29 +0600

    New belongs to creator relationship in Thread

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use App\Reply;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,5 +11,10 @@ class Thread extends Model
     function replies()
     {
     	return $this->hasMany(Reply::class)->latest();
+    }
+
+    function creator()
+    {
+    	return $this->belongsTo(User::class, 'user_id');
     }
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -21,5 +23,16 @@ class ThreadTest extends TestCase
 
 		$this->assertInstanceOf(HasMany::class, $thread->replies());
 		$this->assertEquals(5, $thread->replies()->count());
+	}
+
+	/**
+	 * @test
+	 * a thread has a creator
+	 */
+	public function a_thread_has_a_creator()
+	{
+		$thread = factory('App\Thread')->create();
+
+		$this->assertInstanceOf(User::class, $thread->creator);
 	}
 }
