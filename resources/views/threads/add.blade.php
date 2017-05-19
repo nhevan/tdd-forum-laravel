a496b64e58e7de4e4c7f210a251582030bc9bad9
NH Evan <nhevan@gmail.com>
2017-05-19 15:17:06 +0600

    Using a view composer to remove queries from views.

--- resources/views/threads/add.blade.php
+++ resources/views/threads/add.blade.php
@@ -14,7 +14,7 @@
                             <label for="channel" class="col-sm-2 control-label">Channel:</label>
                             <select name="channel_id" id="channel" class="form-control" required="required">
                                 <option value="">Choose a channel</option>
-                                @foreach (App\Channel::all() as $channel)
+                                @foreach ($channels as $channel)
                                     <option value="{{$channel->id}}">{{$channel->name}}</option>
                                 @endforeach
                             </select>
