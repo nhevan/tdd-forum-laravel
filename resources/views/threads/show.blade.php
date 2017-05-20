a29a0bb30cc91aad7d3986279477021edbcdcdb8
NH Evan <nhevan@gmail.com>
2017-05-20 15:24:04 +0600

    Update thread show view to add a sidebar that contains the thread's metadata.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <a href="#">{{$thread->creator->name }}</a> posted :
@@ -14,13 +14,15 @@
                    {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
             @include ('replies.add')
             @include ('threads.replies')
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <p>This was posted {{$thread->created_at->diffForHumans()}} by <a href="#">{{$thread->creator->name}}</a>. This thread has {{$thread->replies_count}} {{ str_plural('comment', $thread->replies_count) }}.</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
