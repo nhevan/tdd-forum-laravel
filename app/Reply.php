79d2a642717fa8f80de29df4ea5e607b9775f534
NH Evan <nhevan@gmail.com>
2017-05-22 21:37:24 +0600

    ->isFavorite() no more executes additional queries which solves the n+1 issue. Use  property in Reply model.

--- app/Reply.php
+++ app/Reply.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
 	protected $guarded = [];
+	protected $with = ['owner', 'favorites'];
 
 	function owner()
 	{
@@ -25,6 +26,6 @@ class Reply extends Model
 
 	public function isFavorite()
 	{
-		return $this->favorites()->where('user_id', auth()->id())->exists();
+		return !! $this->favorites->where('user_id', auth()->id())->count();
 	}
 }
